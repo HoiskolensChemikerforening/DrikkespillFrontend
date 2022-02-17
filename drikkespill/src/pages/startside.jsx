6f525414072cde9e620f4cb2b86afd06c30dba37
Kristiane Feldmann <kristianevf@gmail.com>
2022-02-17 14:15:23 +0100

    create startpage

---
+++ drikkespill/src/pages/startside.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Startside = () => (
+    <>
+        <h>Hei!</h>
+        <startPageBox>slkjf</startPageBox>
+    </>
+);
+
+const startPageBox = styled.button`
+    color: grey;
+    border: 4px;
+ `;
