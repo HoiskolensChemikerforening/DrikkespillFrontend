81bde742d97046887575dc1caa10c3ee45b49360
Kristiane Feldmann <kristianevf@gmail.com>
2022-02-24 14:12:11 +0100

    loginside

--- drikkespill/src/App.js
+++ drikkespill/src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import React from "react";
 import styled from "styled-components";
 import { Startside } from './pages/startside';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Loginside } from './pages/loginside';
 
 function App() {
   return (
-    <div>
-      <Startside/>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Loginside/>} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
