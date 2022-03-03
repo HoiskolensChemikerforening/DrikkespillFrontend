0b1c7e0f56ac12f1f8f94eb44f2f0365e6df7668
Håkon Fosså <hakfossa@gmail.com>
2022-03-03 12:48:03 +0100

    endringer spillside 0303

--- drikkespill/src/pages/spillside.jsx
+++ drikkespill/src/pages/spillside.jsx
@@ -7,13 +7,29 @@ export const Spillside = () => (
     <>
         <MobileContainer >
             <Background>
-            <Tekstboks>
-            Spørsmåljkasdhfjasdh sdlfjalsdjhfasd
-            dsfhasdlhf
-            adsklfjjhsdaf
-            lasdhflasdjhflkasdf
-            asdfkl
-            </Tekstboks>
+
+            
+            <Information>
+            <Question>
+                <Textbox>
+                Et veldig morsomt spørsmål som noen skal svare på
+                </Textbox>
+            </Question>
+
+            <CurrentPlayer>
+                <Textbox>
+                Det er Håkon sin tur til å velge
+                </Textbox>
+            </CurrentPlayer>
+            </Information>
+
+
+            <Category>
+                <Textbox>
+                Basic    
+                </Textbox>    
+            </Category>
+
             </Background>    
         </MobileContainer>
     </>
@@ -22,18 +38,59 @@ export const Spillside = () => (
 const Background = styled.div`
     width: 100%;
     height: 100%;
-    background-color: #f0f0f0;
-`
+    background-color: #d0d0d0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: column;
+    
 
-const Tekstboks = styled.div`
-    color: black;
-    font-size: 3vmin;
-    text-align: center;
-    width: 50%;
-    padding-right: 25%;
-    padding-left: 25%;
-    padding-top: 15%;
-    padding-bottom: 60%;
+
 `;
 
+const Textbox = styled.div`
+    color: black;
+    font-size: 3vmax;
+    text-align: center;
+    
+`;
 
+const Information = styled.div`
+    
+    height = 50%;
+    display: flex;
+    flex-direction: column;
+
+`
+
+const Question = styled.div`
+    width: 80%;
+    height: 10%;
+    background-color: #ffffff;
+    margin: 5px;
+    padding-top: 10%;
+    
+    
+
+`
+
+const Category = styled.div`
+    width: 80%;
+    height: 5%;
+    background-color: #ffffff;
+    
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-end;
+    
+    margin: 10px;
+
+`;
+
+const CurrentPlayer = styled.div`
+    width: r80%;
+    height: r10%;
+    background-color: #ffffff;
+
+    margin: 10px;
+`
