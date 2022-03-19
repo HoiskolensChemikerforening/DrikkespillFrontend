faa420755af159b19188703ce998d610472a2fb2
Håkon Fosså <hakfossa@gmail.com>
2022-03-19 13:57:59 +0100

    endringer mobilecontainer

--- drikkespill/src/Components/MobileContainer.jsx
+++ drikkespill/src/Components/MobileContainer.jsx
@@ -3,10 +3,7 @@ import styled from 'styled-components';
 const MobileContainer = styled.div`
     width: 390px;
     height: 812px;
-    background-color: lightgrey;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around; 
+
     `;
 
 export {MobileContainer};
