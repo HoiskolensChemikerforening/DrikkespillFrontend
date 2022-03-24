8509fa8136a3bfbc29053d0b5a2c90ab4acbdbca
Håkon Fosså <hakfossa@gmail.com>
2022-03-24 14:08:09 +0100

    random questions

--- drikkespill/src/pages/spillside.jsx
+++ drikkespill/src/pages/spillside.jsx
@@ -1,38 +1,55 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import styled from "styled-components";
 import { MobileContainer } from "../components/MobileContainer";
 import './spillside.css';
-import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, child, get } from "firebase/database";
+
 
 export const Spillside = () => {
     
-    /* const getUsers = async () => {
+    const [users, setUsers] = useState([]);
+    const [questions, setQuestions] = useState([]);
+    
+
+
+    useEffect(() =>{
+        getUsers();
+        getQuestions();
+      },[]);
+    
+    const getUsers = async () => {
         const response = await fetch(
-          `https://drikkespill-c7188-default-rtdb.europe-west1.firebasedatabase.app/drikkespill/bruker`
+          `https://drikkespill-c7188-default-rtdb.europe-west1.firebasedatabase.app/drikkespill/bruker.json`
         );
-        const users = await response.json();
-        console.log(users.hits);
-      } */
-    
-    
-    const firebaseConfig = {
-        apiKey: "AIzaSyAs6zRmpXmZlmmDeCtOFeulxo4v0qPtIeA",
-        authDomain: "drikkespill-c7188.firebaseapp.com",
-        databaseURL: "https://drikkespill-c7188-default-rtdb.europe-west1.firebasedatabase.app",
-        projectId: "drikkespill-c7188",
-        storageBucket: "drikkespill-c7188.appspot.com",
-        messagingSenderId: "879998514120",
-        appId: "1:879998514120:web:fd38ab2f8c6014bb42c6fd",
-        measurementId: "G-63HY1WKNT8"
-    };
-      
-      // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    const database = getDatabase(app);
+        const data = await response.json();
+        setUsers(data);
+        console.log(data);
+    }
 
-    
 
+    const getQuestions = async () => {
+        const response = await fetch(
+          `https://drikkespill-c7188-default-rtdb.europe-west1.firebasedatabase.app/drikkespill/question.json`);
+        const data = await response.json();
+        setQuestions(data);
+        console.log(data);
+    }
+
+
+    const randomQuestion = async () => {
+        let length = questions.length;
+        const random = Math.floor((Math.random()*(length-1))+1);
+        const question = questions[random];
+        const display = document.getElementById("question");
+        display.textContent = question
+    }
+
+    const setName = (button) => {
+        console.log("setting name")
+        const display = document.getElementById(button);
+        console.log()
+        /* console.log(users.044.kallenavn); */
+
+    }
     
     return(
 
@@ -43,8 +60,8 @@ export const Spillside = () => {
             
             <Information>
             <Question>
-                <Textbox>
-                Et veldig morsomt spørsmål som noen skal svare på
+                <Textbox id="question">
+                    question
                 </Textbox>
             </Question>
 
@@ -59,19 +76,18 @@ export const Spillside = () => {
                 <Button className="hei" onClick={() => console.log('1')}>
                     1
                 </Button>
-                <Button onClick={() => console.log('2')}>
+                <Button id="2" onClick={() => console.log('2')}>
                     2
                 </Button>
-                <Button onClick={() => console.log('3')}>
+                <Button id="3" onClick={() => console.log('3')}>
                     3
                 </Button>
-                <Button onClick={() => console.log('4')}>
+                <Button id="4" onClick={() => setName("4")}>
                     4
                 </Button>
-                <Button className="knapp5" onClick={() => console.log('5')}>
+                <Button id="5" className="knapp5" onClick={() => console.log(randomQuestion())}>
                     5
                 </Button>
-                <button className="test" /* onClick={() => getUsers()} */> test </button>
             </Buttons>
 
 
@@ -122,7 +138,7 @@ const Question = styled.div`
     height: 50%;
     background-color: #ffffff;
     margin: 5px;
-    padding-top: 10%;
+    padding-top: 5%;
 ` 
 
 
@@ -141,7 +157,7 @@ const Category = styled.div`
 
 const CurrentPlayer = styled.div`
     width: 80%;
-    height: 30%;
+    height: 50%;
     background-color: #ffffff;
     margin: 10px;
 `
@@ -165,3 +181,29 @@ const Button  = styled.button`
 
 `
 
+
+/* const getUsers = async () => {
+        const response = await fetch(
+          `https://drikkespill-c7188-default-rtdb.europe-west1.firebasedatabase.app/drikkespill/bruker`
+        );
+        const users = await response.json();
+        console.log(users.hits);
+      } */
+    
+    
+    /* const firebaseConfig = {
+        apiKey: "AIzaSyAs6zRmpXmZlmmDeCtOFeulxo4v0qPtIeA",
+        authDomain: "drikkespill-c7188.firebaseapp.com",
+        databaseURL: "https://drikkespill-c7188-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "drikkespill-c7188",
+        storageBucket: "drikkespill-c7188.appspot.com",
+        messagingSenderId: "879998514120",
+        appId: "1:879998514120:web:fd38ab2f8c6014bb42c6fd",
+        measurementId: "G-63HY1WKNT8"
+    };
+      
+      // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const database = getDatabase(app);
+
+     */
